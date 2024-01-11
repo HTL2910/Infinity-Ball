9eb4f9fc83dabbe9b778678b5e0de4a363f3e8d8
HTL2910 <hathanhlong2910@gmail.com>
2024-01-11 11:54:46 +0700

    add

--- Assets/Scripts/BallBounce.cs
+++ Assets/Scripts/BallBounce.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BallBounce : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public int level = 1;
+    //public GameObject ball;
     Vector3 lastVelocity;
     private void Awake()
     {
@@ -14,11 +14,31 @@ public class BallBounce : MonoBehaviour
     private void Update()
     {
         lastVelocity = rb.velocity;
+        CheckVelocity();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        var speed=lastVelocity.magnitude;
+        var speed=lastVelocity.magnitude*1.025f;
         var direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
         rb.velocity = direction * Mathf.Max(speed, 0f);
     }
+    private void CheckVelocity()
+    {
+        if(rb.velocity.x >15f || rb.velocity.x<-15f )
+        {
+            rb.velocity = new Vector2(5f,5f);
+        }
+        if (rb.velocity.y > 15f || rb.velocity.y < -15f)
+        {
+            rb.velocity = new Vector2(5f, 5f);
+        }
+        if (rb.velocity.y <1f && rb.velocity.y> -1f || rb.velocity.x < 1f && rb.velocity.x > -1f)
+        {
+            GameObject gameObj=Instantiate(gameObject, Vector3.zero, Quaternion.identity);
+            gameObj.name = "ball";
+            Destroy(gameObject);
+            
+        }
+
+    }    
 }
